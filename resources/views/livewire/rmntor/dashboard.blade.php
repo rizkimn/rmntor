5b944ef9cceb03d8eb66c10de80fb9d118e8ccd8
rizkimn <rizkimnur0@gmail.com>
2024-08-16 05:24:17 +0900

    Enhance responsivity of layouts

--- resources/views/livewire/rmntor/dashboard.blade.php
+++ resources/views/livewire/rmntor/dashboard.blade.php
@@ -1,17 +1,17 @@
 <x-slot:toolbar>
     <div class="cta-btn grid grid-flow-col gap-2">
-        <a href="#download" class="download-btn flex items-center justify-between px-4 py-2 bg-primary/10 border border-primary rounded-2xl">
-            <span class="text text-sm">Documents</span>
-            <span class="icon ml-4">
+        <a href="#download" class="download-btn flex items-center justify-between md:px-4 p-2 bg-primary/10 border border-primary rounded-2xl">
+            <span class="max-md:hidden text text-sm mr-4">Documents</span>
+            <span class="icon">
                 <svg width="20" height="20" viewBox="0 0 0.6 0.6" xmlns="http://www.w3.org/2000/svg">
                     <path class="fill-primary" fill-rule="evenodd" d="M.525.4A.025.025 0 0 1 .55.422v.053A.075.075 0 0 1 .479.55H.125A.075.075 0 0 1 .05.479V.425A.025.025 0 0 1 .1.422v.053A.025.025 0 0 0 .122.5h.353A.025.025 0 0 0 .5.478V.425A.025.025 0 0 1 .525.4M.3.05a.025.025 0 0 1 .025.025v.24L.357.283A.025.025 0 0 1 .39.281l.002.002a.025.025 0 0 1 .002.033L.392.318.317.393.316.394.314.395.311.397.308.398.305.399.301.4H.295L.292.399.289.398.287.397.285.395.283.393.208.318A.025.025 0 0 1 .241.281l.002.002.032.032v-.24A.025.025 0 0 1 .3.05"/>
                 </svg>
             </span>
         </a>
         <div x-data="{ selector_active: false }" class="selector">
-            <button @click="selector_active = true" class="select-btn flex items-center justify-between px-4 py-2 pr-1 bg-main/10 border border-main rounded-2xl">
-                <span class="text text-sm">Select</span>
-                <span class="icon ml-4">
+            <button @click="selector_active = true" class="select-btn flex items-center justify-between px-4 p-2 pr-1 bg-main/10 border border-main rounded-2xl">
+                <span class="text text-sm mr-4">Select</span>
+                <span class="icon">
                     <svg width="20" height="20" viewBox="0 0 0.375 0.375" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path class="fill-main" fill-rule="evenodd" clip-rule="evenodd" d="M.105.155a.01.01 0 0 1 .016 0l.067.067.067-.067a.011.011 0 0 1 .016.016L.196.246a.01.01 0 0 1-.016 0L.105.171a.01.01 0 0 1 0-.016"/>
                     </svg>
@@ -36,8 +36,8 @@
         </div>
     </div>
     <div x-data="{search_active: false}" class="search-btn">
-        <button @click="search_active = true" class="bg-white/50 border-2 border-white pr-2 rounded-full flex items-center justify-between">
-            <span class="text-left w-60 text-sm py-2 px-4 opacity-50">Temukan sesuatu...</span>
+        <button @click="search_active = true" class="bg-white/50 border-2 border-white p-2 md:px-4 md:pr-2 max-md:rounded-2xl rounded-full flex items-center justify-between">
+            <span class="max-md:hidden text-left w-60 text-sm opacity-50">Temukan sesuatu...</span>
             <span>
                 <svg width="20" height="20" viewBox="0 0 0.6 0.6" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path class="fill-main" fill-rule="evenodd" clip-rule="evenodd" d="M.275.125a.15.15 0 1 0 0 .3.15.15 0 0 0 0-.3m-.2.15a.2.2 0 1 1 .358.123l.085.085a.025.025 0 0 1-.035.035L.398.433A.2.2 0 0 1 .075.275"/>
@@ -46,7 +46,7 @@
         </button>
 
         <div x-show="search_active" class="modal absolute bg-dark/50 left-0 top-0 w-full h-full flex flex-col items-center">
-            <div class="box-modal bg-white w-[36%] p-4 rounded-[36px] mt-36">
+            <div class="box-modal bg-white max-md:w-[86%] w-[36%] p-4 rounded-[36px] mt-36">
                 <form class="bg-white border border-main pr-2 flex items-center justify-between rounded-full overflow-hidden" action="#" method="get">
                     <input name="search" autofocus class="bg-none text-sm w-full px-4 py-2 outline-none" type="text" placeholder="temukan sesuatu...">
                     <button type="submit">
--- resources/views/livewire/rmntor/sidebar.blade.php
+++ resources/views/livewire/rmntor/sidebar.blade.php
@@ -1,14 +1,14 @@
-<aside class="p-4 w-[316px]">
-    <nav class="logo flex justify-center">
+<aside class="max-md:p-1 p-4 max-md:w-auto w-[316px]">
+    <nav class="logo flex max-md:mt-2 max-md:justify-start justify-center">
         <a href="/dashboard#">
-            <span class="flex items-center justify-center logo-image h-16 aspect-square object-contain overflow-hidden rounded-xl">
+            <span class="flex items-center max-md:justify-start justify-center logo-image max-md:h-12 h-16 aspect-square object-contain overflow-hidden rounded-xl">
                 <img class="w-full h-full" src="{{asset("icon.png")}}" alt="rmntor-icon">
             </span>
         </a>
     </nav>
 
     <nav x-data="{ open: false }" class="user-profile group flex flex-col items-center mt-6">
-        <ul class="profile w-full p-[6px] bg-main/10 rounded-2xl overflow-hidden">
+        <ul class="profile max-md:w-auto w-full max-md:p-1 p-[6px] md:bg-main/10 max-md:rounded-[14px] rounded-2xl overflow-hidden">
             <li class="user h-10 flex items-center">
                 <span class="icon flex items-center justify-center">
                     <svg width="28" height="28" viewBox="0 0 24 24" data-name="Line Color" xmlns="http://www.w3.org/2000/svg" class="icon line-color">
@@ -16,9 +16,9 @@
                         <circle class="fill-main/50" cx="12" cy="7" r="4" style="stroke-linecap:round;stroke-linejoin:round;stroke-width:2"/>
                     </svg>
                 </span>
-                <span class="text name ml-1">Le Minerale</span>
+                <span class="max-md:hidden text name ml-1">Le Minerale</span>
             </li>
-            <li x-show="open" x-transition class="user h-10 flex items-center hover:bg-danger/10 rounded-xl">
+            <li x-show="open" x-transition class="user h-10 flex items-center hover:bg-danger/10 max-md:rounded-[10px] rounded-xl">
                 <form class="w-full" action="/logout" method="post"> @csrf @method('DELETE')
                     <button class="flex items-center cursor-pointer w-full" type="submit">
                         <span class="icon flex items-center justify-center p-1">
@@ -26,7 +26,7 @@
                                 <path class="stroke-danger" d="M14 4h3.5c3.058 0 3 4 3 8s.058 8-3 8H14M3 12h12M3 12l4-4m-4 4 4 4" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                             </svg>
                         </span>
-                        <span class="text name ml-1">Logout</span>
+                        <span class="max-md:hidden text name ml-1">Logout</span>
                     </button>
                 </form>
             </li>
@@ -46,7 +46,7 @@
     </nav>
 
     <nav class="menu-group mt-4">
-        <menu class="grid grid-flow-row gap-[6px]">
+        <menu class="grid grid-flow-row gap-[6px] max-md:place-content-center">
             <li>
                 <a class="flex items-center" href="#">
                     <span class="icon flex items-center justify-center p-2 bg-main/10 rounded-xl">
@@ -54,7 +54,7 @@
                             <path class="fill-main" fill-rule="evenodd" clip-rule="evenodd" d="M.07.025H.069L.054.026a.04.04 0 0 0-.028.028L.025.069v.063l.001.015a.04.04 0 0 0 .028.027l.015.001h.063L.147.174A.04.04 0 0 0 .174.146L.175.131V.068L.174.053A.04.04 0 0 0 .146.026L.131.025zM.06.05h.08a.01.01 0 0 1 .009.009v.08A.01.01 0 0 1 .14.148Q.14.151.13.15H.06A.01.01 0 0 1 .051.141L.05.13V.06A.01.01 0 0 1 .059.051M.245.025H.244L.229.026a.04.04 0 0 0-.028.028L.2.069v.063l.001.015a.04.04 0 0 0 .028.027l.015.001h.063L.322.174A.04.04 0 0 0 .349.146L.35.131V.068L.349.053A.04.04 0 0 0 .321.026L.306.025zM.235.05h.08a.01.01 0 0 1 .009.009v.08a.01.01 0 0 1-.009.009h-.08A.01.01 0 0 1 .226.139L.225.13V.06A.01.01 0 0 1 .234.051M.069.2h.063l.015.001a.04.04 0 0 1 .027.028l.001.015v.063L.174.322a.04.04 0 0 1-.028.027L.131.35H.068L.053.349A.04.04 0 0 1 .026.321L.025.306V.243L.026.228A.04.04 0 0 1 .054.201zM.07.225H.06a.01.01 0 0 0-.009.009L.05.245v.07a.01.01 0 0 0 .009.009h.08A.01.01 0 0 0 .148.315v-.08A.01.01 0 0 0 .139.226L.13.225zM.245.2H.244L.229.201a.04.04 0 0 0-.028.028L.2.244v.063l.001.015a.04.04 0 0 0 .028.027L.244.35h.063L.322.349A.04.04 0 0 0 .35.321L.351.306V.243L.35.228A.04.04 0 0 0 .321.201L.306.2zm-.01.025h.08a.01.01 0 0 1 .009.009v.08a.01.01 0 0 1-.009.009h-.08A.01.01 0 0 1 .226.314L.225.305v-.07A.01.01 0 0 1 .234.226"/>
                         </svg>
                     </span>
-                    <span class="text name ml-2">Dashboard</span>
+                    <span class="max-md:hidden text name ml-2">Dashboard</span>
                 </a>
             </li>
             <li>
@@ -64,7 +64,7 @@
                             <path class="fill-main" fill-rule="evenodd" clip-rule="evenodd" d="M8 2h4.5a.5.5 0 0 1 .5.5V5H8zM7 5V2H2.5a.5.5 0 0 0-.5.5V5zM2 6v3h5V6zm6 0h5v3H8zm0 4h5v2.5a.5.5 0 0 1-.5.5H8zm-6 2.5V10h5v3H2.5a.5.5 0 0 1-.5-.5m-1-10A1.5 1.5 0 0 1 2.5 1h10A1.5 1.5 0 0 1 14 2.5v10a1.5 1.5 0 0 1-1.5 1.5h-10A1.5 1.5 0 0 1 1 12.5z"/>
                         </svg>
                     </span>
-                    <span class="text name ml-2 opacity-35 group-hover:opacity-100 transition-all">Tables</span>
+                    <span class="max-md:hidden text name ml-2 opacity-35 group-hover:opacity-100 transition-all">Tables</span>
                 </a>
             </li>
         </menu>
