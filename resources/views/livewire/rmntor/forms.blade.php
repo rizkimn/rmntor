75dc0b4f9073003f26cdd262eb22a274aecfe2ba
rizkimn <rizkimnur0@gmail.com>
2024-08-29 10:37:42 +0900

    Edit Checkbox Input name attribute

--- resources/views/livewire/rmntor/forms.blade.php
+++ resources/views/livewire/rmntor/forms.blade.php
@@ -1,34 +1,7 @@
 @php
-    $options = [
-                [
-                    'name' => "Option 1",
-                    'slug' => "option-1",
-                ],
-                [
-                    'name' => "Option 2",
-                    'slug' => "option-2",
-                ],
-                [
-                    'name' => "Option 3",
-                    'slug' => "option-3",
-                ],
-                [
-                    'name' => "Option 4",
-                    'slug' => "option-4",
-                ],
-                [
-                    'name' => "Option 5",
-                    'slug' => "option-5",
-                ],
-                [
-                    'name' => "Option 6",
-                    'slug' => "option-6",
-                ],
-            ];
-
-    $options2 = [];
-    for ($i=1; $i <= 4; $i++) {
-        $options2[] = ['name' => 'Pilihan '.$i, 'slug' => 'pilihan-'.$i];
+    $options = [];
+    for ($i=1; $i <= 6; $i++) {
+        $options[] = ['name' => 'Pilihan '.$i, 'slug' => 'pilihan-'.$i];
     }
 @endphp
 
@@ -38,13 +11,17 @@
 
 <section>
     {{-- If your happiness depends on money, you will never be happy with yourself. --}}
-    <form action="#">
+    <form action="" method="GET">
         <ul class="grid grid-cols-3 gap-4">
             <li>
                 <x-rmntor.form.text-input label="Full Name" name="full-name" placeholder="Budi Arie Setiadi" :show-icon="false" />
             </li>
             <li class="flex items-end">
-                <x-rmntor.form.select-input name="select" :options="$options2" label="Pilih Pilihan" />
+                <x-rmntor.form.select-input name="select" :options="$options" label="Pilih Pilihan" name="pilihan" />
+            </li>
+            <li class="flex items-end">
+                <x-rmntor.form.checkbox-input label="Check Input" name="check" />
+                <x-rmntor.form.checkbox-input label="Check Input" name="check" />
             </li>
         </ul>
     </form>
