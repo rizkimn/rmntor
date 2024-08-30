c7eb9bf1eb605ef80677dfb87eb94ebf1f2905a8
rizkimn <rizkimnur0@gmail.com>
2024-08-31 01:57:17 +0900

    Add auto switch between asc and desc parameter on sortable table-column component

--- resources/views/components/rmntor/table/table-column.blade.php
+++ resources/views/components/rmntor/table/table-column.blade.php
@@ -1,8 +1,11 @@
 @props(['label','sortable'])
+@php
+    $isAsc = isset($_GET['sort_'.$label]) && $_GET['sort_'.$label] == "asc";
+@endphp
 
 <span class="flex items-center justify-start">
     @if($sortable)
-    <button type="submit" name="sort_{{$label}}" value="asc"><x-rmntor.icon name="caret-top-bottom-icon" height=16 width=16 /></button>
+    <button type="submit" name="sort_{{$label}}" value="{{ $isAsc ? "desc" : "asc"}}"><x-rmntor.icon name="caret-top-bottom-icon" height=16 width=16 /></button>
     @endif
     <span class="text-xs {{strlen($label) <= 3 ? "uppercase" : "capitalize"}}">{{$label}}</span>
 </span>
