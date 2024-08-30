d15e1bec0c96145ee77f28dceecaae2c2cf666b6
rizkimn <rizkimnur0@gmail.com>
2024-08-30 23:38:02 +0900

    Add Number Input to Form Components

---
+++ app/View/Components/Rmntor/Form/NumberInput.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\View\Components\Rmntor\Form;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class NumberInput extends Component
+{
+    /**
+     * Create a new component instance.
+     */
+    public function __construct(
+        public string $placeholder = 'placeholder',
+        public string $name = 'number',
+        public string $label = 'Number',
+        public string $class = '',
+        public int $min = 0,
+        public int $max = 666,
+        public bool $showIcon = true,
+        public string $icon = 'number-icon',
+    ) {/** */}
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.rmntor.form.number-input');
+    }
+}
--- resources/views/livewire/rmntor/forms.blade.php
+++ resources/views/livewire/rmntor/forms.blade.php
@@ -25,10 +25,13 @@
             <li>
                 <x-rmntor.form.date-input name="date" label="Birth Date" />
             </li>
-            <li class="flex items-end">
+            <li>
+                <x-rmntor.form.number-input name="user_pass" />
+            </li>
+            <li class="pt-6">
                 <x-rmntor.form.select-input name="select" :options="$options" label="Pilih Pilihan" name="pilihan" />
             </li>
-            <li class="flex items-end justify-between">
+            <li class="grid grid-cols-3 gap-2">
                 <x-rmntor.form.checkbox-input label="Check Input 1" name="check[]" value="check_1" index="1" />
                 <x-rmntor.form.checkbox-input label="Check Input 2" name="check[]" value="check_2" index="2" />
                 <x-rmntor.form.checkbox-input label="Check Input 3" name="check[]" value="check_3" index="3" />
